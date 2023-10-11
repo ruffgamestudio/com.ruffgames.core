945ade13337ef3185653b750066baa6a49b3208e
Okan Topal <okan92topal@icloud.com>
2023-10-11 09:31:10 +0300

    core package created

--- Editor/HelperMenu.cs
+++ Editor/HelperMenu.cs
@@ -6,7 +6,7 @@ namespace com.ruffgames.core.Runtime.Scripts.Editor
 #if UNITY_EDITOR
     public class HelperMenu
     {
-        [MenuItem("Tools/ClearStorage", false, 1)]
+        [MenuItem("Data/Clear Data", false, 1)]
         private static void ClearStorage()
         {
             var storage = new JsonStorage();
---
+++ Editor/ProjectInitializer.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Windows;
+
+namespace com.ruffgames.core.Core.Editor
+{
+    public class ProjectInitializer : OdinEditorWindow
+    {
+        private const string CorePackagePath = "Packages/com.ruffgames.core/Dependencies";
+
+        private static readonly List<string> PackagesToImport = new List<string>()
+        {
+            "Core",
+            "DoTween",
+            "Odin",
+            "Epic",
+        };
+        
+        [MenuItem("Ruff Games/Project Initializer")]
+        private static void OpenWindow()
+        {
+            GetWindow<ProjectInitializer>().Show();
+        }
+
+        [Button(ButtonSizes.Large)]
+        public void ImportPackages()
+        {
+            if (!Directory.Exists(CorePackagePath))
+            {
+                EditorUtility.DisplayDialog("Error","Directory doesn't exist. Are you trying to run this script in Packages project?","ok");
+                return;
+            }
+
+            var files = System.IO.Directory.GetFiles(CorePackagePath);
+
+            foreach (var file in files)
+            {
+                foreach (var package in PackagesToImport)
+                {
+                    if (file.Contains(package))
+                    {
+                        Debug.Log("Importing package.."+ file);
+                        AssetDatabase.ImportPackage(file,false);
+                    }
+                       
+                }
+            }
+            
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            
+        }
+    }
+}
